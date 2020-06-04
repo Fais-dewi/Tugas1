e2df027f8a5eed89d796eb020ee8768761b7da3d
Fais-dewi <faisdewif27>
2020-06-04 17:46:10 +0700

    tugas

---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?> ../img/cece.jpg" alt="Marcelina putri" class="rounded-circle shadow">
+    <p>hallo nama saya <?= $data['nama']; ?>,pekerjaan saya adalah <?= $data['pekerjaan'] ?>
+</div>
